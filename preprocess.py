2605aa6eea03fd67300e4a7c40171d148827acd3
AkilanChithra <chithra.sathish.akilan@gmail.com>
2024-09-25 21:51:49 -0500

    merged akBranch to main

--- preprocess.py
+++ preprocess.py
@@ -39,8 +39,6 @@ for file in files:
         print("Looking at", file)
     stream = music21.instrument.partitionByInstrument(music21.converter.parse(file, format="midi"))
 
-    notes: list[(int, float, float)] = []
-
     midiFile = converter.parse(file)
     # keySignature = midiFile.analyze('key')
     # print("key signature: ", keySignature)
@@ -67,149 +65,99 @@ for file in files:
         if part.partName is not None: # and ("Flute" in part.partName or True):            
             # TODO: confirm the part is in 4/4 
             # if not, skip the part
-
             notes: list[music21.note.Note | music21.note.Rest] = []
-        notes_and_chords = part.flat.getElementsByClass(['Note', 'Chord'])
-        
+            notes_and_chords = part.flat.getElementsByClass(['Note', 'Chord'])
 
-
-
-
-
-        #Start of Find Key signature of specific part
-
-
-
-
-        if len(notes_and_chords) > 0:
-            try:
-                # Try to analyze the key
-                key_signature = notes_and_chords.analyze('key')
-                print(f"Key Signature: {key_signature}")
-            except DiscreteAnalysisException as e:
-                # Handle the specific DiscreteAnalysisException
-                print(f"Failed to analyze key signature: {str(e)}")
-            except Exception as e:
-                # Handle any other exceptions
-                print(f"An error occurred: {str(e)}")
-        else:
-            print("No notes or chords found in this part of the MIDI file.")
-
-        
-        key_signatures = set([str(key) for key in part.flat.getElementsByClass('KeySignature')])
-
-
-        print("inside main keySignature Area")
-        if len(key_signatures) > 1:
-                print("Multiple key signatures found in this part:")
-                #when this code runs, sometimes returns two of the same key signature
-                # use a set for python.
-                #need to remove these parts from everything
-                for ks in key_signatures:
-                    print(f"Key: {ks}")
-        elif len(key_signatures) == 1:
-            key_signatures = list(key_signatures)
-            print(f"Single key signature: {key_signatures[0]}")
-        else:
-            print("No explicit key signature found. Likely inferred.")
-
-
-
-
-
-
-
-
-
-
-
-
-        #End of key signature code
-
-        if part.partName is not None and ("Flute" in part.partName or True):
-            myNotes = []
-            shouldSkip = False
-            
-            # Fix the melody, then add it to melodyData
-            
-            # TODO: find a way to find the key signature of a given part
-            # TODO: subtract the highest root node above the lowest note in a melody
-
-            def cleanUpMelody():
-                global notes
+            if part.partName is not None and ("Flute" in part.partName or True):
+                shouldSkip = False
                 
-                # Do not try to add a melody if there are no notes
-                if len(notes) <= 16:
-                    return
-                # cut overlapping notes short
-                for n in range(len(notes) - 1):
-                    if((notes[n].offset + notes[n].duration.quarterLength) > notes[n+1].offset):
-                        notes[n].duration.quarterLength = notes[n+1].offset - notes[n].offset
-                melody = []
-                for note in notes:
-                    if isinstance(note, music21.note.Rest):
-                        melody.append((0, note.duration.quarterLength))
-                    else:
-                        melody.append((note.pitch.midi, note.duration.quarterLength))
-                melodyData.append(melody)
-                if display_output:
-                    print("Added melody of", len(melody), "notes from", part.partName)
-                notes = []
-            
-            for note in part.notesAndRests:
+                # #Start of Find Key signature of specific part
+                # if len(notes_and_chords) > 0:
+                #     try:
+                #         # Try to analyze the key
+                #         key_signature = notes_and_chords.analyze('key')
+                #         print(f"Key Signature: {key_signature}")
+                #     except DiscreteAnalysisException as e:
+                #         # Handle the specific DiscreteAnalysisException
+                #         print(f"Failed to analyze key signature: {str(e)}")
+                #     except Exception as e:
+                #         # Handle any other exceptions
+                #         print(f"An error occurred: {str(e)}")
+                # else:
+                #     print("No notes or chords found in this part of the MIDI file.")
 
-                #remove melodies that contain notes with odd fractions
-                dur = (note.duration.quarterLength).as_integer_ratio()
+                # Use a set to remove duplicates
+                key_signatures = set([str(key) for key in part.flat.getElementsByClass('KeySignature')])
 
-                if (not(note.duration.quarterLength % 0.25 == 0) and (not(dur[1] == 3))):
-                    #print(note.duration.quarterLength)
-                    shouldSkip = True
-                    break
-                
-                # record rests as '0'
-                if isinstance(note, music21.note.Rest):
-                    # Try splitting the melody if whole rest
-                    if abs(note.duration.quarterLength - 4.0) < TOLERANCE:
-                        cleanUpMelody()
-                    else:
-                        notes.append(note)
-                elif isinstance(note, music21.note.Note):
-                    notes.append(note)
+                if len(key_signatures) > 1:
+                        print("Multiple key signatures found in this part:")
+                        for ks in key_signatures:
+                            print(f"Key: {ks}")
+                elif len(key_signatures) == 1:
+                    key_signatures = list(key_signatures)
+                    print(f"Single key signature: {key_signatures[0]}")
                 else:
-                    # if a chord is found, skip the entire part
-                    shouldSkip = True
-                    break
+                    print("No explicit key signature found. Likely inferred.")
+
+
+                # Fix the melody, then add it to melodyData
                 
-            if not shouldSkip:
-                cleanUpMelody()
+                # TODO: find a way to find the key signature of a given part
+                # TODO: subtract the highest root node above the lowest note in a melody
 
-        #cut overlapping notes short
-            for n in range(len(myNotes) - 1):
-                if(  (myNotes[n].offset + myNotes[n].duration.quarterLength) > myNotes[n+1].offset):
-                    myNotes[n].duration.quarterLength = myNotes[n+1].offset - myNotes[n].offset
+                def cleanUpMelody():
+                    global notes
+                    
+                    # Do not try to add a melody if there are no notes
+                    if len(notes) <= 16:
+                        return
+                    # cut overlapping notes short
+                    for n in range(len(notes) - 1):
+                        if((notes[n].offset + notes[n].duration.quarterLength) > notes[n+1].offset):
+                            notes[n].duration.quarterLength = notes[n+1].offset - notes[n].offset
+                    melody = []
+                    for note in notes:
+                        if isinstance(note, music21.note.Rest):
+                            melody.append((0, note.duration.quarterLength))
+                        else:
+                            melody.append((note.pitch.midi, note.duration.quarterLength))
+                    melodyData.append(melody)
+                    if display_output:
+                        print("Added melody of", len(melody), "notes from", part.partName)
+                    notes = []
+                
+                for note in part.notesAndRests:
 
-            print(part.partName, "is good:", len(myNotes), "notes")
+                    #remove melodies that contain notes with odd fractions
+                    dur = (note.duration.quarterLength).as_integer_ratio()
 
-    noteData.append(notes)
-
+                    if (not(note.duration.quarterLength % 0.25 == 0) and (not(dur[1] == 3))):
+                        #print(note.duration.quarterLength)
+                        shouldSkip = True
+                        break
+                    
+                    # record rests as '0'
+                    if isinstance(note, music21.note.Rest):
+                        # Try splitting the melody if whole rest
+                        if abs(note.duration.quarterLength - 4.0) < TOLERANCE:
+                            cleanUpMelody()
+                        else:
+                            notes.append(note)
+                    elif isinstance(note, music21.note.Note):
+                        notes.append(note)
+                    else:
+                        # if a chord is found, skip the entire part
+                        shouldSkip = True
+                        break
+                    
+                if not shouldSkip:
+                    cleanUpMelody()
 
     #remove high octave pitches 
  
-    # print(noteData)
-    integers = [note[0] for sublist in noteData for note in sublist]
-    integers = [num for num in integers if num != 0]
-    min_pitch = min(integers)
-    max_pitch = max(integers)
-    pitch_range = max_pitch - min_pitch
-
-    print(pitch_range)
-
-
-
+# print(noteData)
 
 #Code to remove 3+ octaves
-
-
 # maxVal = -100
 # minVal = 2000
 # for note in notes:
@@ -233,59 +181,16 @@ for file in files:
     #     print("smaller")
 
 
+#remove high octave pitches 
 
-
-
-
-
-
-
-
-    #remove high octave pitches 
- 
-    # print(noteData)
-    integers = [note[0] for sublist in noteData for note in sublist]
+for melody in melodyData:
+    integers = [note[0] for note in melody]
     integers = [num for num in integers if num != 0]
     min_pitch = min(integers)
     max_pitch = max(integers)
     pitch_range = max_pitch - min_pitch
 
     print(pitch_range)
-
-
-
-
-#Code to remove 3+ octaves
-
-
-# maxVal = -100
-# minVal = 2000
-# for note in notes:
-#     if len(note) > 0:  # Ensure the tuple has at least one element
-#         if note[0]>maxVal:
-#             maxVal = note[0]
-#         if note[0]<minVal:
-#             minVal = note[0]
-#     else:
-#         print("Tuple is empty or missing the integer part")
-# print(maxVal, minVal)
-    #remove 3+ octave files
-    # print(notes[0])
-    # minPitch = min(notes[0])
-    # maxPitch = max(notes[0])
-    # range = maxPitch-minPitch
-
-    # if range>36:
-    #     print("bigger")
-    # else:
-    #     print("smaller")
-
-
-
-
-
-
-
 
 # i want a list of notes in the following format:
 # [(pitch number, duration in beats), (pitch number)...]
