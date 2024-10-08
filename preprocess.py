e4da0397a550991910ad5e749f46ef3307c92854
myaltaccountsthis <71832770+myaltaccountsthis@users.noreply.github.com>
2024-10-07 20:03:42 -0500

    monday

--- preprocess.py
+++ preprocess.py
@@ -11,11 +11,11 @@ import time
 
 # load files from all given folders
 read_all_files = False
-folders_to_load = ["Mono-Melodies-All/Flute","Mono-Melodies-All/Clarinet","Mono-Melodies-All/Choir Aahs", "Mono-Melodies-All/Alto Sax", "Mono-Melodies-All/Acoustic Guitar", "Mono-Melodies-All/Acoustic Grand Piano"] if read_all_files else ["Mono-Melodies-All/Acoustic Grand Piano"]
+folders_to_load = ["Mono-Melodies-All/Flute","Mono-Melodies-All/Clarinet","Mono-Melodies-All/Choir Aahs", "Mono-Melodies-All/Alto Sax", "Mono-Melodies-All/Acoustic Guitar", "Mono-Melodies-All/Acoustic Grand Piano"] if read_all_files else ["Mono-Melodies-All/Flute"]
 
 files = []
 
-file_limit = -1 if read_all_files else 25
+file_limit = -1 if read_all_files else 10
 file_count = 0
 note_count = 0
 display_output = False
@@ -50,9 +50,6 @@ def durationToString(duration: float | Fraction) -> str:
 if display_file_output:
     print(" " * 100)
 
-output_file = open("out/melodyDataRunning.txt", "w")
-output_file.write("[")
-
 # isolate the flute part
 # or any single melody
 melodyData: list[list[(int, float)]] = []
@@ -65,30 +62,6 @@ for file_index in range(file_count):
         print(f"\033[F\33[2K\r[{"#" * progress_int}{"-" * (PROGRESS_CHARACTERS - progress_int)}] {progress:.1f}% - {file[:last_slash] + "/" + file[last_slash + 1:last_slash+7] + "..."} ({len(melodyData)} melodies, {note_count} notes)")
     try:
         stream = music21.instrument.partitionByInstrument(music21.converter.parse(file, format="midi"))
-
-        '''
-        midiFile = converter.parse(file)
-        keySignature = midiFile.analyze('key')
-        print("key signature: ", keySignature)
-
-        parts = midiFile.parts
-        if parts:
-            if len(notes_and_chords) > 0:
-                try:
-                    # Try to analyze the key
-                    key_signature = notes_and_chords.analyze('key')
-                    print(f"Key Signature: {key_signature}")
-                except DiscreteAnalysisException as e:
-                    # Handle the specific DiscreteAnalysisException
-                    print(f"Failed to analyze key signature: {str(e)}")
-                except Exception as e:
-                    # Handle any other exceptions
-                    print(f"An error occurred: {str(e)}")
-            else:
-                print("No notes or chords found in this part of the MIDI file.")
-        else:
-            print("no parts found")
-        '''
 
         for part in stream.parts:
             if part.partName is not None:
@@ -195,14 +168,9 @@ for file_index in range(file_count):
                     # offset is largest root that is <= min_pitch
                     offset = min_pitch % 12 - root
                     offset = offset if offset <= 0 else offset - 12
-                    if display_output:
-                        print(counts, root)
-                    melody = [(note[0] - (min_pitch + offset) + 60, note[1]) if note[0] != 0 else note for note in melody]
+                    melody = [(note[0] - (min_pitch + offset) + 1, note[1]) if note[0] != 0 else note for note in melody]
                     
-                    if (len(melodyData) > 0):
-                        output_file.write(", ")
                     note_count += len(melody)
-                    output_file.write(str(melody))
                     melodyData.append(melody)
                     if display_output:
                         print("Added melody of", len(melody), "notes from", part.partName)
@@ -235,29 +203,10 @@ for file_index in range(file_count):
     except Exception as e:
         if display_file_output:
             print(f"Error reading {file}: {e}\n")
- 
-"""
-# i want a list of notes in the following format:
-# [(pitch number, duration in beats), (pitch number)...]
-# pitch number is the midi number of the pitch
-# eg: C3 is 60, C4 is 72, C5 is 84
-
-noteDict = {}
-noteDictLen = 0
-
-def one_hot_encode(note: tuple[int, float], noteDict: dict): 
-    if note not in noteDict:
-        noteDict[note] = noteDictLen
-        noteDictLen += 1
-    return noteDict[note]
-
-one_hot_encode()"""
 
 # output the note data to a file
 with open("out/melodyData.txt", "w") as file:
     file.write(str(melodyData))
-output_file.write("]")
-output_file.close()
 
 print("Wrote to file successfully!")
 print(f"Finished running in {time.time() - startTime:.3f} seconds")
