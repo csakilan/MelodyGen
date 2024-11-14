6f17180f241e7c8adf5a9a5e201cd1f9e86495d2
Sanjana <sanjana.ravikumar77@gmail.com>
2024-11-13 21:18:46 -0600

    comments so i don't go crazy

--- backend/server.py
+++ backend/server.py
@@ -23,15 +23,15 @@ model = tf.keras.models.Sequential([
 
 model.load_weights("checkpoints/model_full_v1.h5")
 
-
-@app.route('/predict', methods=['POST'])
+#waiting for a post request in editor.tsx
+@app.route('/predict', methods=['POST']) # source is predict, goes to this file
 def predict():
     # Receive data from the front end
     # fetch("http://127.0.0.1:5000/predict", {method:"POST", headers: {"Content-Type": "application/json" }, body: JSON.stringify({notes: [ [1, "1.00"], [3, '1.00'], [5, '1.00'], [6, '1.00'] ]})}).then(res => res.json()).then(console.log)
     # Seems to prefer ('C', 'C'), probably since it's the most common
     data = request.json
     print(data)
-    # Convert newNotes to model input format
+    # Convert newNotes to model input format -> actual note generation
     newNotes = [encodings[tuple(note)] for note in data["notes"]]
     addedNotes = []
     X = []
